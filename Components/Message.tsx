ae7daee3275e2040c3460b771215f47d69f93a8b
uniquemozilla22 <uniqgaming26@gmail.com>
2022-09-12 01:43:27 +0545

    added sender and Receiver css

--- Components/Message.tsx
+++ Components/Message.tsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import styled from 'styled-components'
+import { auth } from '../firebase'
 
-const Message = ({user, message}) => {
+const Message = ({user, message}:any) => {
 
-    console.log("Message component",user,message)
+  const [userLoggedIn] = useAuthState(auth)
+
+  const TypeofMessage =  user === userLoggedIn?.email ? Sender : Receiver
+
   return (
-    <p>{message.message}</p>
+    <Container>
+      <TypeofMessage>
+        {message.message}
+        </TypeofMessage></Container>
   )
 }
+
+
+const Container =  styled.div``;
+
+
+const MessageElement = styled.p`
+width:fit-content;
+padding:15px;
+border-radius:8px;
+margin:10px;
+min-width:80px;
+padding-bottom:26px;
+position:relative;
+text-align:right;`;
+
+
+const Sender = styled(MessageElement)`
+  margin-left:auto;
+  background-color: #dcf8c6;
+
+`
+
+
+const Receiver = styled(MessageElement)`
+  background-color:whitesmoke;
+  text-align:left;
+`
+
 
 export default Message
